b3829b69e948706b8facb273039f02a776a97a15
Sheza Munir <sheza.cs.toronto.edu>
2026-07-30 14:29:18 -0400

    nonelicitation scenario B run

--- pilot_study.py
+++ pilot_study.py
@@ -242,7 +242,7 @@ def init_participant(prolific_pid=None, study_id=None, session_id=None) -> dict:
         # "resumed_at": None,
         # "paused_at": None,
         # "scenario_id": random.choice(SCENARIOS)["id"], # uncomment this for main study
-        "scenario_id": "B",
+        "scenario_id": "A",
         "workflow_stage": "disclosure",
         "disclosure": {},
         "elicitation": [],
